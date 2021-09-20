7ae0569ec34f197c3cd410c9e93d85b8eb1c4032
Don Vest <tsukasasan@hotmail.com>
2021-09-20 15:54:48 -0500

    Created and completed JS Basics Exercise

---
+++ script.js
@@ -0,0 +1,58 @@
+// 1
+console.log(7);
+
+// 2
+// 5 MAIN PRIMITIVES 
+// 1) Numbers
+// 2) Strings
+// 3) Booleans
+// 4) Null
+// 5) Undefined
+
+// 3
+// SIX OPERATOR SYMBOLS
+// + - * / ** %
+
+// 4
+console.log(5%4);
+console.log(5%2);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\ALL!');
+
+// 6
+console.log("Hawk"+"eye");
+
+// 7
+console.log("Don".length+"Vest".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// Bonus 1
+console.log("\\");
+
+// 2
+console.log(5**4-9);
+
+// 3
+console.log(-"I".length + "Am".length - "Iron".length + "Man".length);
+
+// 3.2 Extra Bonus
+console.log(-"I".length - "Am".length % "Iron".length + "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+var m=25
+var c=4
+var u= m+c
+console.log(u);
